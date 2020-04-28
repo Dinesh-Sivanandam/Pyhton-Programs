f0b7787ac99cf7e41e1a1f002d692f99005a6417
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2020-04-28 08:54:15 +0530

    Swap_Case in pythin

---
+++ sWAP_cASE.py
@@ -0,0 +1,22 @@
+def swap_case(S):
+    import string
+
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    
+
+    T = []
+    for i in range(len(S)):
+        if S[i] in lower:
+            T.append(S[i].upper())
+        elif S[i] in upper:
+            T.append(S[i].lower())
+        else:
+            T.append(S[i])
+    return (''.join(T))
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print (result)
+
