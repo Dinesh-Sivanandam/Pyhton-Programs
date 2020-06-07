8d113636f6a4268f1c6156ecab324abc40147e31
Dinesh <61511078+Dinesh-Sivanandam@users.noreply.github.com>
2020-06-07 16:34:23 +0530

    Matrix Multiplication of 1D and 2D array
    The above program gives the statements for multiplication for 1 and 2D arrays using numpy.

---
+++ Multiplication.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+#One dimensional array
+a=np.array([1,2,3])
+b=np.array([4,5,6])
+print(np.multiply(a,b))
+print()
+
+#Two dimensional array
+c=np.array([[1,2,3],
+           [4,5,6],
+           [7,8,9]])
+d=np.array([[9,8,7],
+           [6,5,4],
+           [3,2,1]])
+print(np.multiply(c,d))
